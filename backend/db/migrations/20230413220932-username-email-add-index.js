d2698d7fb02b48f6a50b4dcfd8ae13b51d060b77
j1jlee <enoneddih1989@gmail.com>
2023-04-13 18:34:55 -0400

    Add Users table migration and seeds

---
+++ backend/db/migrations/20230413220932-username-email-add-index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addIndex('Users', ['username'], { unique: true});
+    await queryInterface.addIndex('Users', ['email'], { unique: true});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeIndex('Users', ['username']);
+    await queryInterface.removeIndex('Users', ['email']);
+  }
+};
---
+++ backend/db/models/user.js
@@ -0,0 +1,54 @@
+'use strict';
+const { Model, Validator } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      notNull: true,
+      unique: true,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error("Cannot be an email.")
+          }
+        }
+      }
+    },
+      email: {
+        type: DataTypes.STRING,
+        notNull: true,
+        unique: true,
+        validate: {
+          len: [3, 256],
+          isEmail: true
+          // isEmail(value) {
+          //   if (!Validator.isEmail(value)) {
+          //     throw new Error("Must be an email.")
+          //   }
+          // }
+        }
+    },
+    hashedPassword: {
+      type:DataTypes.STRING,
+      notNull: true,
+      validate: {
+        len: [60, 60]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
